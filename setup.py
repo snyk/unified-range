52641e3cbf2e8548fecdd7e66f576a549c4232a4
tomersnyk <tomer@snyk.io>
2018-09-17 09:57:45 +0300

    chore: refactor setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='unified_range',
-      version='0.1dev',
+      version='0.0.1-dev',
       packages=['unified_range', ],
       license='MIT License',
-      long_description=open('README.md').read(), )
+      long_description=long_description, )

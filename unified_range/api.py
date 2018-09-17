05ab4e0873fcecc7b17a5b7e291c9bdda3d293f3
tomersnyk <tomer@snyk.io>
2018-09-17 14:26:27 +0300

    fix: improve api

--- unified_range/api.py
+++ unified_range/api.py
@@ -3,16 +3,30 @@ from unified_range import models
 
 
 def from_semver(semver_spec):
+    """
+    Convert semver string to unified range string.
+    :param semver_spec: str
+    :return: unified_spec
+    """
     ver_rng = utils.create_from_semver(semver_spec)
-    return ver_rng
+    return str(ver_rng)
 
 
 def to_semver(spec):
+    """
+    Convert unified range string to semver string.
+    :param spec: str
+    :return: semver
+    """
     semver = utils.transform_to_semver(spec)
     return semver
 
 
-# fixme: REMOVE!
-def unified_version(spec):
-    v = models.UnifiedVersionRange.create_from_spec(spec)
-    return v
+def unified_range(spec):
+    """
+    Return VersionRange for unified range.
+    Only support unified range format.
+    :param spec: str
+    :return: VersionRange instance
+    """
+    return models.UnifiedVersionRange.create_from_spec(spec)

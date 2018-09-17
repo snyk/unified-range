72c8ef82650e6680221e393d454481d35a4c49a0
tomersnyk <tomer@snyk.io>
2018-09-17 09:44:16 +0300

    chore: packaging required files

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='unified_range',
+      version='0.1dev',
+      packages=['unified_range', ],
+      license='MIT License',
+      long_description=open('README.md').read(), )

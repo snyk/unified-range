9b3e139784629c1f470be256974a70365f3f7df6
tomersnyk <tomer@snyk.io>
2018-11-13 19:50:03 +0200

    chore: update version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='unified_range',
     description="Convert between semver range and maven version range",
     url="https://github.com/snyk/unified-range",
-    version='0.0.3',
+    version='0.0.4',
     packages=['unified_range', ],
     license='MIT License',
     long_description=long_description,
